5b60ccbe449c6590d22b3d63915855dd3f6d0ce0
Vlad Tatarinskiy <ghoust187@gmail.com>
2024-08-19 15:27:06 +0300

    2

--- Source.cpp
+++ Source.cpp
@@ -8,26 +8,46 @@ private:
     int size;
 
 public:
-    // Конструктор, принимающий количество элементов
+   
     SmartArray(int initialCapacity) : capacity(initialCapacity), size(0) {
         if (initialCapacity <= 0) {
-            throw std::invalid_argument("Должна быть положительна");
+            throw std::invalid_argument("Должна быть положительной");
         }
         data = new int[capacity];
     }
 
-    // Деструктор
+   
     ~SmartArray() {
         delete[] data;
     }
 
-    // Функция добавления нового элемента
+ 
+    SmartArray(const SmartArray& other) : capacity(other.capacity), size(other.size) {
+        data = new int[capacity];
+        for (int i = 0; i < size; ++i) {
+            data[i] = other.data[i];
+        }
+    }
+
+    
+    SmartArray& operator=(const SmartArray& other) {
+        if (this != &other) {
+            delete[] data; 
+            capacity = other.capacity;
+            size = other.size;
+            data = new int[capacity];
+            for (int i = 0; i < size; ++i) {
+                data[i] = other.data[i];
+            }
+        }
+        return *this;
+    }
+
+  
     void push_back(int value) {
         if (size == capacity) {
-            // Увеличиваем емкость в два раза
             capacity *= 2;
             int* newData = new int[capacity];
-            // Копируем старые данные
             for (int i = 0; i < size; ++i) {
                 newData[i] = data[i];
             }
@@ -37,7 +57,7 @@ public:
         data[size++] = value;
     }
 
-    // Функция получения элемента по индексу
+  
     int at(int index) const {
         if (index < 0 || index >= size) {
             throw std::out_of_range("Out of bounds");
@@ -48,40 +68,31 @@ public:
 
 int main() {
     try {
+        setlocale(LC_ALL, "Russian");
+
+
+        SmartArray arr(5);
+        arr.push_back(1);
+        arr.push_back(4);
+        arr.push_back(155);
+
+        SmartArray new_array(2);
+        new_array.push_back(44);
+        new_array.push_back(34);
+
         
-        SmartArray myArray(5);
+        arr = new_array;
 
        
-        myArray.push_back(10);
-        myArray.push_back(20);
-        myArray.push_back(30);
-        myArray.push_back(40);
-        myArray.push_back(50);
-        myArray.push_back(60); 
-
-        
-        std::cout << "Элемент с индексом 0: " << myArray.at(0) << std::endl;
-        std::cout << "Элемент с индексом 2: " << myArray.at(2) << std::endl;
-
+        std::cout << "Elements of arr: ";
+        for (int i = 0; i < sizeof(arr); ++i) {
+            std::cout << arr.at(i) << " ";
+        }
+        std::cout << std::endl;
     }
     catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
-
-
-    SmartArray arr(5);
-    arr.push_back(1);
-    arr.push_back(4);
-    arr.push_back(155);
-
-    SmartArray new_array(2);
-    new_array.push_back(44);
-    new_array.push_back(34);
-
-    arr = new_array;
-
-    std::cout << "Элемент с индексом 0: " << arr.at(0) << std::endl;
-    std::cout << "Элемент с индексом 2: " << arr.at(2) << std::endl;
 
     return 0;
 }
